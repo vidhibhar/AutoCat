7e5a0f83ab712333250fb696dbf802b86eb64cc3
vidhibhar <109489963+vidhibhar@users.noreply.github.com>
2022-07-17 19:32:48 -0700

    adding some api calls

---
+++ autocat/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color=""
+      light
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="AutoCat Logo"
+          class=""
+          contain
+          src="./assets/AUTOCAT_2.png"
+          transition="scale-transition"
+          width="150"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">SEARCH CATALOG</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <SearchPage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SearchPage from './components/SearchPage';
+export default {
+  name: 'App',
+
+  components: {
+    SearchPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+
+</style>
